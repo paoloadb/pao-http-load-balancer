3f7559d8a9123b099963598715c54b753e78d244
paoloadb <paoloadb@yahoo.com>
2023-03-23 02:08:01 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"net/http"
+	"net/http/httputil"
+)
+
+var (
+servers = []string {
+	"http://localhost:9001",
+	"http://localhost:9000",
+	"http://localhost:9002",
+}
+index = 0
+globalCounter = 0
+)
+
+func main() {
+
+	go func() {
+		fmt.Println(len(servers), " servers.")
+		http.HandleFunc("/", forwardRequest)
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
+fmt.Scanln()
+}
+
+func forwardRequest(res http.ResponseWriter, req *http.Request) {	
+	fmt.Println("globalcounter is at ", globalCounter)
+	if globalCounter == 3 {
+		globalCounter = 0
+	}
+		u := getServerFromList()
+		target := httputil.NewSingleHostReverseProxy(u)
+		target.ServeHTTP(res, req)
+	globalCounter +=1
+}
+
+func getServerFromList() *url.URL {
+	ctr := index % len(servers)
+	fmt.Println("counter at ", ctr)
+	x, err := url.Parse(servers[ctr])
+	if err != nil {
+		panic(err)
+	}
+	index++
+	fmt.Println("Serving", servers[ctr])
+	return x
+}
